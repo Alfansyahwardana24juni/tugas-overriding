a3c8c4d4dbd407954bc56507b2fbebf4be33b7c2
Alfansyahwardana24juni <alfansyahwardanaalfa@gmail.com>
2024-01-25 09:22:19 +0800

    ini sudah private semua

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
 
-
 class Produk
 {
-    public $judul, $penulis, $penerbit;
-    protected $diskon = 0;
-    private $harga;
+    private $judul, $penulis, $penerbit, $diskon = 0, $harga;
 
     public function __construct($judul = 'judul', $penulis = 'penulis', $penerbit = 'penerbit', $harga = '0')
     {
@@ -15,16 +12,48 @@ class Produk
         $this->penerbit  = $penerbit;
         $this->harga     = $harga;
     }
+    // set
+    public function setJudul($judul)
+    {
+        $this->judul = $judul;
+    }
+    // get
+    public function getJudul()
+    {
+        return $this->judul;
+    }
+    // set
+    public function setPenulis($penulis)
+    {
+        $this->penulis = $penulis;
+    }
+    // get
+    public function getPenulis()
+    {
+        return $this->penulis;
+    }
+    // set
+    public function setPenerbit($penerbit)
+    {
+        $this->penerbit = $penerbit;
+    }
+    // get 
+    public function getPenerbit()
+    {
+        return $this->penerbit;
+    }
 
+    // set
     public function setDiskon($diskon)
     {
         $this->$diskon = $diskon;
     }
-
+    // get
     public function getHarga()
     {
         return $this->harga - ($this->harga * $this->diskon / 100);
     }
+    // get
     public function getLabel()
     {
         return "$this->penulis, $this->penerbit";
